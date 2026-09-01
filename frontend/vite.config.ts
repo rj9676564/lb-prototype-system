df5d7c2c4e3ec64dd222354e99dab3321f6dbba6
laibin <laibin6@gmail.com>
2026-09-01 09:40:55 +0800

    feat: host frontend static assets on GitHub Pages CDN

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: process.env.VITE_CDN_URL || "/",
   plugins: [react()],
   server: {
     host: true,
